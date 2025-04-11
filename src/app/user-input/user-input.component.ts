a0a4ed4c722d46d0fa401322918475a52ce5e466
urke90 <urosbijelic90@gmail.com>
2025-04-11 17:02:30 +0200

    feat: reander inital form add values and 2 way binding with on submit handler

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+  imports: [FormsModule],
+})
+export class UserInputComponent {
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 0;
+  duration = 0;
+
+  onSubmit() {
+    const data = {
+      initialInvestment: this.initialInvestment,
+      annualInvestment: this.annualInvestment,
+      expectedReturn: this.expectedReturn,
+      duration: this.duration,
+    };
+    console.log('form submitted', data);
+  }
+}
